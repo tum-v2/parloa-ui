a1c9868fd08f66be2a9916d2f908a881edf3cbd9
Ozan Aydın <ozan.aydin@precipoint.de>
2023-11-07 14:15:16 +0100

    Add eslint/recommended config, fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  var x;
   return (
     <main className={styles.main}>
       <div className={styles.description}>
