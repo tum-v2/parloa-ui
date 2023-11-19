7d5d89b29f4bdeb6ee4b720241479e1c36b8292b
Robyn Kölle <robyn.koelle@tum.de>
2023-11-19 04:18:05 +0100

    Add theme properties

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -6,15 +6,35 @@ const lexend = Lexend({
 });
 
 const theme = {
+  color: {
+    primary: '#3f2547',
+    gray: '#7a7a7a'
+  },
+  padding: {
+    xs: '4px',
+    s: '8px',
+    m: '12px',
+    l: '24px',
+    xl: '48px'
+  },
+  borderRadius: {
+    xs: '4px',
+    s: '8px',
+    m: '12px',
+    l: '24px'
+  },
+  /**
+   *  AntD:
+   **/
   token: {
     colorSuccess: '#0aad6a',
     colorInfo: '#3f2547',
     colorWarning: '#f6b033',
     colorError: '#f63535',
     colorTextBase: '#24132b',
+    borderRadiusXS: 4,
     borderRadius: 8,
-    borderRadiusXS: 8,
-    borderRadiusLG: 12,
+    borderRadiusLG: 24,
     colorPrimary: '#3f2547',
     colorBorder: '#dddae0',
     fontFamily: lexend.style.fontFamily
@@ -24,9 +44,6 @@ const theme = {
       colorInfo: 'rgb(202, 93, 217)',
       colorInfoBg: 'rgb(251, 234, 255)',
       colorInfoBorder: 'rgb(234, 177, 249)'
-    },
-    Tag: {
-      borderRadiusSM: 24
     }
   }
 };
