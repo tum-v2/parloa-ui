8b95ca02e41fe27a9ea812a49251804e151d4d8e
Robyn Kölle <robyn.koelle@tum.de>
2023-11-19 04:34:44 +0100

    Add pill component

---
+++ src/components/generic/Pill.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Tag } from 'antd';
+import theme from '@/theme/theme';
+
+interface PillProps {
+  icon?: React.ReactNode;
+  color?: string;
+  width?: string;
+  children?: React.ReactNode;
+}
+
+const Pill: React.FC<PillProps> = ({
+  icon,
+  color = theme.color.primary,
+  width = undefined,
+  children
+}) => {
+  const pillStyle: React.CSSProperties = {
+    borderRadius: 50,
+    width: width || 'max-content',
+    maxWidth: '150px',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    paddingTop: theme.padding.xs,
+    paddingLeft: theme.padding.m,
+    paddingRight: theme.padding.m,
+    paddingBottom: theme.padding.xs
+  };
+
+  return (
+    <Tag style={pillStyle} color={color} icon={icon} bordered={false} >
+      {children}
+    </Tag>
+  );
+};
+
+export default Pill;
