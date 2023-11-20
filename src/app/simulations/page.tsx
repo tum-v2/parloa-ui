9e7716730833f5a3adda2227c99c3649df283c59
Sathwik Amburi <sathwik.amburi@tum.de>
2023-11-20 16:22:18 +0100

    input field done!

--- src/app/simulations/page.tsx
+++ src/app/simulations/page.tsx
@@ -1,11 +1,31 @@
 'use client';
 import InputField from '@/components/generic/InputField';
+import { BsFillSendFill } from 'react-icons/bs';
+import { Flex } from 'antd';
 // eslint-disable-next-line require-jsdoc
 export default function Page() {
   return (
     <>
       <h1>Hello, Simulations Page!</h1>
-      <InputField placeholder="This is a sample input" size="large" />
+      <Flex vertical gap={32}>
+        <InputField
+          type="text"
+          placeholder="This is a sample input"
+          size="large"
+          suffix={<BsFillSendFill />}
+        />
+        <InputField
+          type="textarea"
+          placeholder="This is a sample textarea"
+          size="large"
+          rows={4}
+        />
+        <InputField
+          type="password"
+          placeholder="This is a sample password"
+          size="large"
+        />
+      </Flex>
     </>
   );
 }
--- src/components/generic/InputField.tsx
+++ src/components/generic/InputField.tsx
@@ -2,35 +2,76 @@ import React from 'react';
 import { Input } from 'antd';
 
 interface InputFieldProps {
+  type: 'text' | 'password' | 'textarea';
   size?: 'small' | 'middle' | 'large';
   placeholder?: string;
   suffix?: React.ReactNode;
   id?: string;
   value?: string;
-  type?: string;
   onChange?: () => void;
   onPressEnter?: () => void;
   maxLength?: number;
   disabled?: boolean;
   showCount?: boolean;
+  rows?: number;
+  iconRender?: () => React.ReactNode;
 }
+
+const { TextArea } = Input;
+const { Password } = Input;
 // eslint-disable-next-line require-jsdoc
 export default function InputField(props: InputFieldProps) {
-  return (
-    <>
-      <Input
-        size={props.size}
-        placeholder={props.placeholder}
-        suffix={props.suffix}
-        id={props.id}
-        value={props.value}
-        type={props.type}
-        onChange={props.onChange}
-        onPressEnter={props.onPressEnter}
-        maxLength={props.maxLength}
-        disabled={props.disabled}
-        showCount={props.showCount}
-      />
-    </>
-  );
+  if (props.type === 'textarea') {
+    return (
+      <>
+        <TextArea
+          rows={props.rows}
+          placeholder={props.placeholder}
+          id={props.id}
+          value={props.value}
+          onChange={props.onChange}
+          onPressEnter={props.onPressEnter}
+          maxLength={props.maxLength}
+          disabled={props.disabled}
+          showCount={props.showCount}
+        />
+      </>
+    );
+  } else if (props.type === 'password') {
+    return (
+      <>
+        <Password
+          size={props.size}
+          placeholder={props.placeholder}
+          suffix={props.suffix}
+          id={props.id}
+          value={props.value}
+          onChange={props.onChange}
+          onPressEnter={props.onPressEnter}
+          maxLength={props.maxLength}
+          disabled={props.disabled}
+          showCount={props.showCount}
+          iconRender={props.iconRender}
+        />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Input
+          size={props.size}
+          placeholder={props.placeholder}
+          suffix={props.suffix}
+          id={props.id}
+          value={props.value}
+          type={props.type}
+          onChange={props.onChange}
+          onPressEnter={props.onPressEnter}
+          maxLength={props.maxLength}
+          disabled={props.disabled}
+          showCount={props.showCount}
+        />
+      </>
+    );
+  }
 }
