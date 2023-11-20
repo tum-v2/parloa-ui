49d714a495089694a0fd9f88c667668c942d1b0d
Robyn Kölle <robyn.koelle@tum.de>
2023-11-20 19:52:43 +0100

    Use same height for buttons with and without an icon

--- src/components/generic/Button.tsx
+++ src/components/generic/Button.tsx
@@ -12,7 +12,7 @@ const Button = ({ icon, onClick, children }: ButtonProps) => {
   const buttonStyle: React.CSSProperties = {
     borderRadius: theme.borderRadius.m,
     width: icon && !children ? '50px' : 'max-content',
-    height: icon ? '50px' : '40px',
+    height: '50px',
     boxShadow: 'none' // Remove the default shadow of the AntD component.
   };
 
