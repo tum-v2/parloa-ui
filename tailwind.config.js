bd362defcc31cdd384265fae07439245f4b2af4a
Robyn Kölle <robyn.koelle@tum.de>
2023-11-22 11:19:03 +0100

    Disable tailwind preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  preflight: false,
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [],
+  corePlugins: {
+    preflight: false
+  }
 };
