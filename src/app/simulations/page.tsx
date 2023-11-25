ac8ed919c0b3fb89be16e50c5ad490ffac0e458c
Sathwik Amburi <sathwik.amburi@tum.de>
2023-11-25 18:44:59 +0100

    Simulation Card Component Created

--- src/app/simulations/page.tsx
+++ src/app/simulations/page.tsx
@@ -1,20 +1,11 @@
 'use client';
 import SimulationModal from '@/components/simulation-modal/SimulationModal';
-import SimulationCard from '@/components/simulation-modal/SimulationCard';
 // eslint-disable-next-line require-jsdoc
 export default function Page() {
   return (
     <>
       <h1>Hello, Simulations Page!</h1>
       <SimulationModal />
-      <SimulationCard
-        mode="manual"
-        onClick={() => console.log('Manual Simulation Card Clicked')}
-      />
-      <SimulationCard
-        mode="automated"
-        onClick={() => console.log('Automated Simulation Card Clicked')}
-      />
     </>
   );
 }
--- src/components/simulation-modal/StepContent.tsx
+++ src/components/simulation-modal/StepContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from '../generic/Button';
-
+import SimulationCard from './SimulationCard';
 interface StepContentProps {
   stepNumber: number;
   enterWildStep: () => void;
@@ -12,6 +12,45 @@ const StepContent: React.FC<StepContentProps> = ({
   enterWildStep,
   isLastStep
 }) => {
+  const renderContentForStep = () => {
+    switch (stepNumber) {
+      case 1:
+        return (
+          <div style={{ display: 'flex' }}>
+            <div style={{ padding: '20px' }}>
+              <SimulationCard
+                mode="manual"
+                onClick={() => console.log('Manual Simulation Card Clicked')}
+              />
+            </div>
+            <div style={{ padding: '20px' }}>
+              <SimulationCard
+                mode="automated"
+                onClick={() => console.log('Automated Simulation Card Clicked')}
+              />
+            </div>
+          </div>
+        );
+
+      case 2:
+        // Content for step 2
+        break;
+
+      case 3:
+        // Content for step 3
+        return isLastStep ? (
+          <Button onClick={enterWildStep}>Go to Wild Step</Button>
+        ) : (
+          // Other content for step 3 if it's not the last step
+          <p>{`Step ${stepNumber}`}</p>
+        );
+
+      default:
+        // Default content if none of the above cases match
+        return <p>{`Step ${stepNumber}`}</p>;
+    }
+  };
+
   return (
     <div
       style={{
@@ -21,11 +60,7 @@ const StepContent: React.FC<StepContentProps> = ({
         justifyContent: 'center'
       }}
     >
-      {isLastStep ? (
-        <Button onClick={enterWildStep}>Go to Wild Step</Button>
-      ) : (
-        <p>{`Step ${stepNumber}`}</p>
-      )}
+      {renderContentForStep()}
     </div>
   );
 };
