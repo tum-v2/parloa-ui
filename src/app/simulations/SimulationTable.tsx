8e053e1ba9dbc5651bf4ca5413220eefc56853e0
GLDuval <glevesqueduval@gmail.com>
2023-11-30 14:50:03 +0100

    Refactor SimulationTable component to display scenario, type, status, createdAt, and updatedAt in a more user-friendly format

--- src/app/simulations/SimulationTable.tsx
+++ src/app/simulations/SimulationTable.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { Simulation } from '@/api/schemas/simulation';
 import useSimulations from '@/hooks/useSimulations';
+import { getSimulationStatusBadgeStatus } from '@/lib/utils/simulations/simulationStyles';
+import { firstLetterToUpperCase, underscoresToSpaces } from '@/lib/utils/text';
+import { Badge } from 'antd';
 import Table, { ColumnsType } from 'antd/es/table';
 import { useRouter } from 'next/navigation';
 
@@ -13,12 +16,49 @@ const columns: ColumnsType<Simulation> = [
   {
     title: 'Scenario',
     dataIndex: 'scenario',
-    key: 'scenario'
+    key: 'scenario',
+    render: (scenario: string) => {
+      return (
+        <span>{firstLetterToUpperCase(underscoresToSpaces(scenario))}</span>
+      );
+    }
   },
   {
     title: 'Type',
     dataIndex: 'type',
-    key: 'type'
+    key: 'type',
+    render: (type: string) => {
+      return <span>{firstLetterToUpperCase(underscoresToSpaces(type))}</span>;
+    }
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+    render: (status: string) => {
+      return (
+        <Badge
+          status={getSimulationStatusBadgeStatus(status)}
+          text={firstLetterToUpperCase(status)}
+        />
+      );
+    }
+  },
+  {
+    title: 'Created At',
+    dataIndex: 'createdAt',
+    key: 'createdAt',
+    render: (createdAt: string) => {
+      return <span>{new Date(createdAt).toLocaleString()}</span>; // TODO: Change to appropiate format (ask client)
+    }
+  },
+  {
+    title: 'Updated At',
+    dataIndex: 'updatedAt',
+    key: 'updatedAt',
+    render: (updatedAt: string) => {
+      return <span>{new Date(updatedAt).toLocaleString()}</span>;
+    }
   }
 ];
 
