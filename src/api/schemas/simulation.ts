b4134bfc84f9546136b2ec9449ac7d730484444f
GLDuval <glevesqueduval@gmail.com>
2023-12-11 13:58:40 +0100

    Add optimization API endpoints and schemas

--- src/api/schemas/simulation.ts
+++ src/api/schemas/simulation.ts
@@ -4,15 +4,16 @@ export const SimulationSchema = z.object({
   _id: z.string(),
   __v: z.number().optional(),
   name: z.string(),
-  scenario: z.string(),
   type: z.enum(['AUTOMATED', 'MANUAL', 'OPTIMIZATION', 'A/B TESTING']),
   numConversations: z.number(),
   totalNumberOfInteractions: z.number(),
+  optimization: z.string().optional().nullable(),
   serviceAgent: z.string(),
   userAgent: z.string(),
   conversations: z.string().array(),
   status: z.string(),
   duration: z.number(),
+  evaluation: z.string().optional(),
   createdAt: z.string().datetime(),
   updatedAt: z.string().datetime()
 });
--- src/api/simulation.ts
+++ src/api/simulation.ts
@@ -1,7 +1,7 @@
 import { SimulationSchema } from './schemas/simulation';
 
 /**
- * /simulation/:id/poll Get simulation
+ * /simulation/:id Get simulation
  */
 export const getSimulation = async (id: string) => {
   const response = await fetch(
