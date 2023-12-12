dd377e5b316afbd0b84dfdea5aab137f768304c6
GLDuval <glevesqueduval@gmail.com>
2023-12-12 12:26:20 +0100

    Fix style and add onClick

--- src/app/dashboard/components/HighYieldSimulationCard.tsx
+++ src/app/dashboard/components/HighYieldSimulationCard.tsx
@@ -4,10 +4,12 @@ import theme from '@/theme/theme';
 import Pill from '@/components/generic/Pill';
 import { IoPerson } from 'react-icons/io5';
 import { formatLongDateTimeString } from '@/lib/utils/dateTime';
+import { useRouter } from 'next/navigation';
 
 const { Text, Title } = Typography;
 
 const skeletonData = [1, 2, 3, 4, 5];
+
 export interface HighYieldSimulationCardProps {
   _id: string;
   name: string;
@@ -17,47 +19,56 @@ export interface HighYieldSimulationCardProps {
 }
 
 const HighYieldSimulationCard = ({
+  _id,
   name,
   createdAt,
   domain,
   successRate
 }: HighYieldSimulationCardProps) => {
-  const titleStyle: React.CSSProperties = {
-    height: '5rem'
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/simulations/details/${_id}`);
   };
 
   return (
     <Card
       style={{
         marginBottom: theme.padding.m,
-        borderRadius: theme.borderRadius.m
+        borderRadius: theme.borderRadius.m,
+        cursor: 'pointer'
       }}
+      onClick={onClick}
+      className="ease-in-out duration-300 hover:backdrop-blur-lg hover:bg-gray-100"
     >
-      <div style={titleStyle}>
-        <Title style={{ margin: 0 }} ellipsis={{ rows: 2 }} level={4}>
-          {name}
-        </Title>
-      </div>
-      <Text style={{ margin: 0, color: theme.color.gray }}>
-        {formatLongDateTimeString(createdAt)}
-      </Text>
-      <Flex justify="space-between" align="center">
-        <Pill>
-          <IoPerson />
-          <Text
-            style={{
-              margin: 0,
-              marginLeft: theme.padding.xs,
-              color: theme.color.white
-            }}
-          >
-            {domain}
+      <Flex justify="space-between" gap={'large'} vertical>
+        <div>
+          <Title style={{ margin: 0 }} ellipsis={{ rows: 1 }} level={4}>
+            {name}
+          </Title>
+          <Text style={{ margin: 0, color: theme.color.gray }}>
+            {formatLongDateTimeString(createdAt)}
           </Text>
-        </Pill>
-        <div></div>
-        <Title style={{ margin: 0 }} level={1}>
-          {Math.trunc(successRate * 100)}%
-        </Title>
+        </div>
+
+        <Flex justify="space-between" align="center">
+          <Pill>
+            <IoPerson />
+            <Text
+              style={{
+                margin: 0,
+                marginLeft: theme.padding.xs,
+                color: theme.color.white
+              }}
+            >
+              {domain}
+            </Text>
+          </Pill>
+          <div></div>
+          <Title style={{ margin: 0 }} level={1}>
+            {Math.trunc(successRate * 100)}%
+          </Title>
+        </Flex>
       </Flex>
     </Card>
   );
