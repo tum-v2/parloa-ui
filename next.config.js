01979421cd72db65c3b911363b339d6953b10819
busrabgz <busrabgz@gmail.com>
2023-12-13 00:35:32 +0100

    Define client side env variables on workflow file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // eslint-disable-next-line tsdoc/syntax
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  env: {
-    NEXT_PUBLIC_SIMULATION_API_URL: process.env.NEXT_PUBLIC_SIMULATION_API_URL,
-  },
+  output: 'standalone'
 };
 
 module.exports = nextConfig;
