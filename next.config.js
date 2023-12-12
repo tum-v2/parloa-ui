5b80c356c8fef4f9a01a743edf5dc579f680c6f3
busrabgz <busrabgz@gmail.com>
2023-12-13 00:22:15 +0100

    Add env variable to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line tsdoc/syntax
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_SIMULATION_API_URL: process.env.NEXT_PUBLIC_SIMULATION_API_URL,
+  },
 };
 
 module.exports = nextConfig;
